4478df724754e8af02a0bf62af5de3837894229e
uttaradeshpande28 <95483902+uttaradeshpande28@users.noreply.github.com>
2023-07-09 19:50:31 +0530

    Update test_generate_excel.py

--- test/test_generate_excel.py
+++ test/test_generate_excel.py
@@ -51,9 +51,8 @@ def test_generate_excel_file():
     for idx_value in idx_values:
         cell_with_image = sheet[f'D{idx_value}']
         image = cell_with_image.value
-        # Check if the cell value is not None
-        assert image is not None
-    
+        assert image is not None and isinstance(image, Image)
+
     # Verify the image file
     # cell_with_image = sheet['D3']
     # image_filename = os.path.basename(cell_with_image.value)
