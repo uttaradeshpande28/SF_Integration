c47f7acc69729408eef917e7a80de05a8eef0bd8
uttaradeshpande28 <95483902+uttaradeshpande28@users.noreply.github.com>
2023-07-09 15:34:31 +0530

    Update test_generate_excel.py

--- test/test_generate_excel.py
+++ test/test_generate_excel.py
@@ -1,11 +1,16 @@
 import os
 from openpyxl import load_workbook
 import pytest
+from generate_excel import generate_excel_file
+
 print("Imports successful test file")
 
 def test_generate_excel_file():
+    # Generate the Excel file
+    generate_excel_file()
+
     # Verify the contents of the current directory
-    print("Contents of the current directory _test:")
+    print("Contents of the current directory:")
     print(os.listdir())
 
     # Verify that the Excel file is generated successfully
@@ -27,35 +32,19 @@ def test_generate_excel_file():
     assert sheet['B3'].value == 'George'
     assert sheet['C3'].value == 'Bluth'
 
-    # Test case for multiple user data entries
-    assert sheet['A4'].value == 'janet.weaver@reqres.in'
-    assert sheet['B4'].value == 'Janet'
-    assert sheet['C4'].value == 'Weaver'
-    # Add more assertions for additional user data entries
+    # Verify the image data
+    assert sheet['D3'].anchor.type == 'oneCellAnchor'  # Verify the image anchor type
 
-    # Test case for missing user data fields
-    assert sheet['A5'].value == 'emma.wong@reqres.in'
-    assert sheet['B5'].value == 'Emma'
-    assert sheet['C5'].value == 'Wong'
-    # Add more assertions for missing user data fields
+    # Verify the image dimensions
+    img = sheet['D3'].image
+    assert img.width == 96
+    assert img.height == 96
 
-    # Test case for special characters in user data
-    assert sheet['A6'].value == 'user1@example.com'
-    assert sheet['B6'].value == 'User with Special Characters'
-    assert sheet['C6'].value == 'Special äëñ characters'
-    # Add more assertions for user data with special characters
+    # Verify the image file format
+    assert img.format == 'jpeg'
 
-    # Test case for large amount of user data
-    assert sheet['A7'].value == 'user1@example.com'
-    assert sheet['B7'].value == 'User1'
-    assert sheet['C7'].value == 'Lastname1'
-    # Add more assertions for large amount of user data
-
-    # Test case for unexpected data types
-    assert sheet['A8'].value == 'charles.morris@reqres.in'
-    assert sheet['B8'].value == 12345  # Unexpected data type: number instead of a first name
-    assert sheet['C8'].value == 'Morris'
-    # Add more assertions for user data with unexpected data types
+    # Verify the cleanup of the local image file
+    assert not os.path.exists('1-image.jpg')
 
     # Clean up the generated Excel file
     os.remove('user_data.xlsx')
