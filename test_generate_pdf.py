5729ed4de2550b2f53267a3c7b154f7021e5716e
uttaradeshpande28 <95483902+uttaradeshpande28@users.noreply.github.com>
2023-07-10 03:06:23 +0530

    Update test_generate_pdf.py

--- test_generate_pdf.py
+++ test_generate_pdf.py
@@ -3,7 +3,7 @@ import requests
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image
 from reportlab.lib import colors
-import generate_pdf.py
+import delete_existing_images
 
 def test_fetch_user_data():
     # Define test data
